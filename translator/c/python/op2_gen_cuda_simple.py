2b0699d3c4e1c1b911609b76ca89a445cc172ec4
Istvan Reguly <regulyistvan@gmail.com>
2020-12-02 09:09:01 +0100

    CUDA codegen patched

--- translator/c/python/op2_gen_cuda_simple.py
+++ translator/c/python/op2_gen_cuda_simple.py
@@ -882,6 +882,7 @@ def op2_gen_cuda_simple(master, date, consts, kernels,sets, macro_defs):
         code('  int part_size = OP_part_size;')
         code('#endif')
         code('')
+      #code('int set_size = op_mpi_halo_exchanges_cuda(set, nargs, args);')
       code('int set_size = op_mpi_halo_exchanges_grouped(set, nargs, args, 2);')
 
 #
@@ -894,6 +895,7 @@ def op2_gen_cuda_simple(master, date, consts, kernels,sets, macro_defs):
       ENDIF()
       code('')
       code('op_mpi_halo_exchanges_grouped(set, nargs, args, 2);')
+      #code('op_mpi_halo_exchanges_cuda(set, nargs, args);')
 
     IF('set->size > 0')
     code('')
@@ -1021,6 +1023,7 @@ def op2_gen_cuda_simple(master, date, consts, kernels,sets, macro_defs):
       FOR('col','0','Plan->ncolors')
       IF('col==Plan->ncolors_core')
       code('op_mpi_wait_all_grouped(nargs, args, 2);')
+      #code('op_mpi_wait_all_cuda(nargs, args);')
       ENDIF()
       code('#ifdef OP_BLOCK_SIZE_'+str(nk))
       code('int nthread = OP_BLOCK_SIZE_'+str(nk)+';')
@@ -1102,6 +1105,7 @@ def op2_gen_cuda_simple(master, date, consts, kernels,sets, macro_defs):
         FOR('round','0','2')
       IF('round==1')
       code('op_mpi_wait_all_grouped(nargs, args, 2);')
+      #code('op_mpi_wait_all_cuda(nargs, args);')
       ENDIF()
       if reduct:
         code('int start = round==0 ? 0 : (round==1 ? set->core_size : set->size);')
